66b31cbe9de37c0508c97bae70179c035a655b90
GlassBricks <24237065+GlassBricks@users.noreply.github.com>
2024-06-29 19:49:33 -0700

    Extract shell.nix

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -8,6 +8,7 @@
 }: {
   imports = [
     ./fonts.nix
+    ./shell.nix
   ];
 
   nixpkgs = {
@@ -81,57 +82,6 @@
   programs.neovim = {
     enable = true;
     defaultEditor = true;
-  };
-
-  programs.fish = {
-    enable = true;
-    shellAliases = {
-      ":wq" = "exit";
-    };
-  };
-  home.sessionPath = [
-    "$HOME/bin"
-  ];
-
-  home.file = {
-    "bin/my-nixos-rebuild" = {
-      enable = true;
-      source = ../rebuild.sh;
-    };
-  };
-
-  programs.bash = {
-    enable = true;
-    enableCompletion = true;
-    bashrcExtra = ''
-      export PATH="$PATH:$HOME/bin:$HOME/.local/bin:$HOME/go/bin"
-    '';
-    initExtra = ''
-      if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
-      then
-        shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
-        exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
-      fi
-    '';
-  };
-
-  programs.kitty = {
-    enable = true;
-    shellIntegration = {
-      enableFishIntegration = true;
-      enableBashIntegration = true;
-    };
-    theme = "Catppuccin-Macchiato";
-    font = {
-      name = "JetBrainsMono Nerd Font Light";
-      size = 10;
-    };
-    extraConfig = ''
-      background_opacity 0.8
-      bold_font JetBrainsMono Nerd Font
-      italic_font JetBrainsMono Nerd Font Light Italic
-      bold_italic_font JetBrainsMono Nerd Font Italic
-    '';
   };
 
   programs.java.enable = true;
---
+++ home-manager/shell.nix
@@ -0,0 +1,54 @@
+# shell, terminal, and custom scripts
+{pkgs, ...}: {
+  home.sessionPath = [
+    "$HOME/bin"
+  ];
+
+  home.file = {
+    "bin/my-nixos-rebuild" = {
+      enable = true;
+      source = ../rebuild.sh;
+    };
+  };
+
+  programs.fish = {
+    enable = true;
+    shellAliases = {
+      ":wq" = "exit";
+    };
+  };
+
+  programs.bash = {
+    enable = true;
+    enableCompletion = true;
+    bashrcExtra = ''
+      export PATH="$PATH:$HOME/bin:$HOME/.local/bin:$HOME/go/bin"
+    '';
+    initExtra = ''
+      if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
+      then
+        shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
+        exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
+      fi
+    '';
+  };
+
+  programs.kitty = {
+    enable = true;
+    shellIntegration = {
+      enableFishIntegration = true;
+      enableBashIntegration = true;
+    };
+    theme = "Catppuccin-Macchiato";
+    font = {
+      name = "JetBrainsMono Nerd Font Light";
+      size = 10;
+    };
+    extraConfig = ''
+      background_opacity 0.8
+      bold_font JetBrainsMono Nerd Font
+      italic_font JetBrainsMono Nerd Font Light Italic
+      bold_italic_font JetBrainsMono Nerd Font Italic
+    '';
+  };
+}
