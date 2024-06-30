d59ebe14b53951a097302739696b42e49eb897c8
GlassBricks <24237065+GlassBricks@users.noreply.github.com>
2024-06-29 18:57:45 -0700

    Always run home manager if nixos config changed

--- rebuild.sh
+++ rebuild.sh
@@ -11,11 +11,13 @@ if [ -n "$(git status --porcelain)" ]; then
 fi
 
 # if changes to nixos folder, run nixos-rebuild
-if git diff --name-only HEAD~1 HEAD | grep -q nixos; then
+#if git diff --name-only HEAD~1 HEAD | grep -q nixos; then
+shouldUpdate=$(git diff --name-only HEAD~1 HEAD | grep nixos)
+if [ -n "$shouldUpdate" ]; then
     sudo nixos-rebuild --flake . switch || exit 1
 fi
 
-# if changes to home-manager folder, run home-manager switch
-if git diff --name-only HEAD~1 HEAD | grep -q home-manager; then
+# if changes to home-manager folder, or nixos rebuild was run, run home-manager switch
+if [ -n "$shouldUpdate" ] || git diff --name-only HEAD~1 HEAD | grep -q home-manager; then
     home-manager switch --flake . || exit 1
 fi
