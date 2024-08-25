15b3cd6b07c9d231fbfa07868a7322c0eba7748c
GlassBricks <24237065+GlassBricks@users.noreply.github.com>
2024-08-24 21:06:02 -0500

    Update factorio

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
 
     # Home manager
     home-manager.url = "github:nix-community/home-manager/release-24.05";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs-unstable";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = {
